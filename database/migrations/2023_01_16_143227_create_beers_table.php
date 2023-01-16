e80a110429ec4027c3b0c5dc23c45f7e760af64a
FR_Jess <fr_jess@icloud.com>
2023-01-16 15:45:53 +0100

    [add] migration

--- database/migrations/2023_01_16_143227_create_beers_table.php
+++ database/migrations/2023_01_16_143227_create_beers_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('beers', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('slug', 150)->unique();
+            $table->float('price', 5,2);
+            $table->float('rating', 3,2)->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
