41bdbb72233980c7f9b35c1534535e02b6ff6f71
FR_Jess <fr_jess@icloud.com>
2023-01-16 16:08:18 +0100

    [create] seeder

--- app/Models/Beer.php
+++ app/Models/Beer.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Beer extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+        $c = 1;
+        $beer_exists = Beer::where('slug', $slug)
+        ->first();
+        while($beer_exists){
+            $slug = $original_slug . '-' . $c;
+            $beer_exists = Beer::where('slug',$slug)
+            ->first();
+            $c++;
+        }
+        return $slug;
+    }
 }
